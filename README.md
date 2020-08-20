5a55c036629781e072258657d5806b7390c315fc
drpott <drpottumati@exemail.com.au>
2020-08-20 11:10:43 +1000

    changed README.md and init.el

--- README.md
+++ README.md
@@ -1,40 +1,36 @@
 # trigger35 using Python 3.x
-network automation using trigger for me
+network automation using trigger and python 3.x for me
 
-## work done so far:
- - fixed tacacsrc and to use only GPG encryption
- - fixed twister.py
- - fixed gong script (telnet and ssh works now)
- - fixed netdevices
- - fixed commands with commando
- - made netdevices leaner (got rid of Prodonly, ACLs - who uses ACLs nowadays)
- - fixed cli pty
+## Technologies and requirements
+Project is created with:
+* Linux
+* emacs IDE
+* python 3.x (any version > 3 should work)
+* gpg2 (for tacacsrc module using gpg encryption)
+* gnupg-agent
 
-## work to do
- - other shit most likely
+## Setup
+To run this project:
+```
+   $ dnf install conda emacs
+   $ conda create -n py37 python=3.7
+   $ ln -s .conda/envs .virtualenvs (this more for emacs venv)
+   $ git clone https://github.com/drpott/trigger35.git
+   $ cd trigger35
+   $ conda activate py37
+   $ pip install twisted crochet IPy pyasn1 SimpleParse pytz cryptography bcrypt
+   $ gpg2 --gen-key (make sure user is 5 chars long and change in settings.py to reflect user/pass)
+```
+   Update /etc/hosts file and py scripts location in: (work in progress, until better method)
+     * bin/*.py,
+     * trigger/config/settings.py and
+     * trigger/conf/__init__.py
 
-## requirements
-
- - Environment:
-   - Linux
-   - python 3.x
-   - gpg2 (gpg2 --gen-key)
-   - gnupg-agent
-
- - Install Procedure
-     dnf install conda emacs
-     conda create -n py37 python=3.7
-     git clone https://github.com/drpott/trigger35.git
-     cd trigger35
-     conda activate py37
-     pip install twisted crochet IPy pyasn1 SimpleParse pytz cryptography bcrypt
-     gpg2 --gen-key (make sure user is 5 chars long and change in settings.py to reflect user/pass)
-
-update /etc/hosts file and py scripts location in bin/*.py, trigger/config/settings.py and trigger/conf/__init__.py
-
-
-  - for emacs:
-    - https://realpython.com/emacs-the-best-python-editor/
-    ln -s .conda/envs .virtualenvs
-    venv-work on py37
-    elpy-config
+## for emacs IDE
+Take init.el from:
+* https://realpython.com/emacs-the-best-python-editor/
+Also in myenviron directory
+```
+    M-x venv-work on py37
+    M-x elpy-config
+```
--- myenviron/emacs_init.el
+++ myenviron/emacs_init.el
@@ -1,76 +1,72 @@
-;; INSTALL PACKAGES
-;; --------------------------------------
+;; .emacs.d/init.el
 
+;; ===================================
+;; MELPA Package Support
+;; ===================================
+;; Enables basic packaging support
 (require 'package)
-(add-to-list 'package-archives                                                                                                        
-             '("elpy" . "http://jorgenschaefer.github.io/packages/"))   
-(add-to-list 'package-archives
-       '("melpa" . "https://melpa.org/packages/") t)
 
-;; activate all packages
+;; Adds the Melpa archive to the list of available repositories
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+
+;; Initializes the package infrastructure
 (package-initialize)
 
-;; fetch the list of packages available 
-(unless package-archive-contents
+;; If there are no archived package contents, refresh them
+(when (not package-archive-contents)
   (package-refresh-contents))
 
-;; define list of packages to install
+;; Installs packages
+;;
+;; myPackages contains a list of package names
 (defvar myPackages
-  '(better-defaults
-    material-theme
-    exec-path-from-shell
+  '(better-defaults                 ;; Set up some better Emacs defaults
     elpy
-    pyenv-mode))
+    flycheck
+    blacken
+    material-theme                  ;; Theme
+    )
+  )
 
-;; install all packages in list
+;; Scans the list in myPackages
+;; If the package listed is not already installed, install it
 (mapc #'(lambda (package)
-    (unless (package-installed-p package)
-      (package-install package)))
+          (unless (package-installed-p package)
+            (package-install package)))
       myPackages)
 
-;; Use shell's $PATH
-(exec-path-from-shell-copy-env "PATH")
 
-;; init.el ends here
-;; If, on startup, packages can't be found, try `M-x pacakge-refresh-contents`.
+;; ===================================
+;; Basic Customization
+;; ===================================
 
-
-;; BASIC CUSTOMIZATION
-;; --------------------------------------
-
-(setq inhibit-startup-message t)   ;; hide the startup message
-(load-theme 'material t)           ;; load material theme
-(global-linum-mode t)              ;; enable line numbers globally
+(setq inhibit-startup-message t)    ;; Hide the startup message
+(load-theme 'material t)            ;; Load material theme
+(global-linum-mode t)               ;; Enable line numbers globally
 (setq linum-format "%4d \u2502 ")  ;; format line number spacing
-;; Allow hash to be entered  
-(global-set-key (kbd "M-3") '(lambda () (interactive) (insert "#")))
-
-;; define list of packages to install
-(defvar myPackages
-  '(better-defaults
-    material-theme
-    elpy))
-
-(elpy-enable)
-
-(add-hook 'python-mode-hook 'jedi:setup)
-(setq jedi:complete-on-dot t)
-
-(pyenv-mode)
-;; (require 'conda)
-;; (setq conda-env-home-directory (expand-file-name "~/.conda/envs"))
-(setq python-shell-interpreter "ipython"
-      python-shell-interpreter-args "-i --simple-prompt")
-(venv-set-location "~/.conda/envs")
-
-(require 'popwin)
-(popwin-mode 1)
-
 (define-key (current-global-map) (kbd "M-o") 'other-window)
 (global-set-key (kbd "S-C-<left>") 'shrink-window-horizontally)
 (global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
 (global-set-key (kbd "S-C-<down>") 'shrink-window)
 (global-set-key (kbd "S-C-<up>") 'enlarge-window)
+
+;; ====================================
+;; Development Setup
+;; ====================================
+
+;; Enable elpy
+(elpy-enable)
+;; (setq elpy-rpc-backend "jedi")
+;; (add-hook 'python-mode-hook 'jedi:setup)
+;; (setq jedi:complete-on-dot t) 
+;; (require 'popwin)
+;; (popwin-mode 1)
+
+;; Enable Flycheck
+(when (require 'flycheck nil t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -79,7 +75,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (jedi-direx rope-read-mode jedi virtualenvwrapper conda pyenv-mode-auto popwin popup-complete auto-complete deferred pyenv-mode elpy exec-path-from-shell material-theme better-defaults))))
+    (blacken flycheck virtualenvwrapper virtualenv pyenv-mode-auto popwin popup-complete material-theme jedi-direx exec-path-from-shell elpy conda company-jedi better-defaults))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
